352e2f7ca9249d49fb8210546bdaee8328633f78
iMac_Zimai <zimai.li@evobio.eu>
2022-04-20 19:01:22 +0200

    Reorganise folder structure

--- Scripts/DataExploration/rawDataExplore.m
+++ Scripts/DataExploration/rawDataExplore.m
@@ -86,26 +86,3 @@ hold on;
 scatter(categorical(antOutput.Treatment), antOutput.InNestRatio, 'black', 'filled','jitter','on','jitterAmount',0.15);
 hold off;
 lme = fitlme(antOutput,'InNestRatio~Treatment+(1|ColonyID)')
-
-%% Plot the trajectory of each ant in every colony
-
-figure;
-for i = 1:numFile
-     xyMatrix = load(fullfile(xyDir, outputFiltered.Filename(i))).xy(1:frameKeep,:); % load xy matrix
-     numAnts = size(xyMatrix, 2)/2; % number of ants equals to number of columns
-     xlim_min = min(min(xyMatrix(:,1:2:2*numAnts-1))); % set x and y axis lim
-     xlim_max = max(max(xyMatrix(:,1:2:2*numAnts-1)));
-     ylim_min = min(min(xyMatrix(:,2:2:2*numAnts)));
-     ylim_max = max(max(xyMatrix(:,2:2:2*numAnts)));
-     for j = 1:numAnts
-        subplot(numFile,maxNumAnts, (i-1)*maxNumAnts+j); % make numFile*maxNumAnts grid plot
-        axis ij;
-        x= xyMatrix(:,2*j-1);
-        y = xyMatrix(:,2*j);
-        % assignment rate
-        scatter(x, y, 0.5, 'filled');
-        xlim([xlim_min,xlim_max]);
-        ylim([ylim_min,ylim_max]);
-     end
-end
-saveas(gcf,'trajectories.png')
