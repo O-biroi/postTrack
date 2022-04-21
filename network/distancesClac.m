d761ee0a048dda78a46eaa5de8e7d437848c495b
Daniel <d.m.knebel@gmail.com>
2022-04-21 16:49:04 +0200

    Updated codes

--- network/distancesClac.m
+++ network/distancesClac.m
@@ -1,23 +1,23 @@
-function [distancesMat] = distances(xy2, inNest, withNest)
+function [distancesMat] = distancesClac(xy2, inNest, withNest)
     % withNest - either 1 or zero;
 
     %% prepare variables
-    distancesMat = cell(size(xy2));         %   make an empty cell array
+    distancesMat = cell(size(xy2));                                         %   make an empty cell array
     for i = 1:1:size(xy2, 1)
         for in = 1:1:size(xy2, 2)
-            xyTemp = xy2{i, in};            %   take XY coordinates of the current experiment and colony
-            xTemp = xyTemp(:, 1:2:end);     %   seperate Xs and Ys
+            xyTemp = xy2{i, in};                                            %   take XY coordinates of the current experiment and colony
+            xTemp = xyTemp(:, 1:2:end);                                     %   seperate Xs and Ys
             yTemp = xyTemp(:, 2:2:end);
 
 
-            inNestTemp = inNest{i, in};     %   take the inNest values for the current experiment and colony
+            inNestTemp = inNest{i, in};                                     %   take the inNest values for the current experiment and colony
 
             xTemp(logical(inNestTemp)) = nan;
             yTemp(logical(inNestTemp)) = nan;
 
-            inNestTemp(inNestTemp == 0) = nan;      % make all not in nest ants nans - for later calculation
+            inNestTemp(inNestTemp == 0) = nan;                              % make all not in nest ants nans - for later calculation
 
-            xSub = [];                      %   make empty variables
+        xSub = [];                                                          %   make empty variables
             ySub = [];
             inNestSub = [];
 
@@ -36,15 +36,17 @@ function [distancesMat] = distances(xy2, inNest, withNest)
                 %             inNestSub = [inNestSub, nan(size(xTemp, 1), ind-1), inNestTemp(:, 1:end-ind+1) ./ inNestTemp(:, ind:end)];      %   all in nest will be 0, all others will be nan
             end
 
-            distancesMatTemp1 = sqrt(xSub.^2 + ySub.^2);    %   elucidian distance
+            distancesMatTemp1 = sqrt(xSub.^2 + ySub.^2);                    %   elucidian distance
             if withNest == 1
-                distancesMatTemp1(inNestSub == 0) = 0;      %   all inNest ants will have 0 distance from each other
+                distancesMatTemp1(inNestSub == 0) = 0;                      %   all inNest ants will have 0 distance from each other
             end
 
             %% make sense of the matrix order
-            distancesMatTemp2 = reshape(distancesMatTemp1', size(inNestTemp, 2), size(inNestTemp, 2), []);      % organize the matrix into a n*n*num-of-frames matrix.
+            distancesMatTemp2 = reshape(distancesMatTemp1', ...
+                size(inNestTemp, 2), size(inNestTemp, 2), []);              % organize the matrix into a n*n*num-of-frames matrix.
             for ind = 1:size(distancesMatTemp2, 1)
-                distancesMatTemp2(ind, 1:ind, :) = fliplr(distancesMatTemp2(ind, 1:ind, :));        % delete the upper half of the matrix
+                distancesMatTemp2(ind, 1:ind, :) = ...
+                    fliplr(distancesMatTemp2(ind, 1:ind, :));               % delete the upper half of the matrix
             end
             distancesMat{i, in} = distancesMatTemp2;
         end
