abf947968a15eb5005b65330837fa974948f0056
Daniel <d.m.knebel@gmail.com>
2022-04-23 19:33:28 +0200

    cluster test2

--- network/inputs.m
+++ network/inputs.m
@@ -1,5 +1,5 @@
-parameters.numOfFrames = %12000*12*3;
-parameters.numOfSegments = %12*3;
+parameters.numOfFrames = 6000; %12000*12*3;
+parameters.numOfSegments = 2%12*3;
 parameters.folderPath = '/shares/ulr-lab/users/daniel/GiacomoData/y';
 
 parameters.withNest = 1;
@@ -8,8 +8,8 @@ parameters.contactThresh = 0.002;
 
 parameters.infectionProbs = [0.001 0.1];
 parameters.infectionProbsJumps = 0.5;%0.001;
-parameters.entryPoints = 1;%1:16;
-parameters.numOfReps = 1;%100;
+parameters.entryPoints = 2;%1:16;
+parameters.numOfReps = 2;%100;
 
 parameters.outputFolderPath = '/shares/ulr-lab/users/daniel/networks/output';
 parameters.fileName = 'young.mat';
