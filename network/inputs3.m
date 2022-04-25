74c13c54b47e38cd292277c4e7aaf964feec9fb0
Daniel <d.m.knebel@gmail.com>
2022-04-25 11:50:05 +0200

    clusterTest3

--- network/inputs3.m
+++ network/inputs3.m
@@ -1,17 +1,17 @@
 parameters.time = datetime;
 
 parameters.numOfFrames = 12000; % within segment
-parameters.numOfSegments = 12*7;
+parameters.numOfSegments = 2;
 parameters.folderPath = '/shares/ulr-lab/users/daniel/GiacomoData/y';
 
 parameters.withNest = 1;
 
 parameters.contactThresh = 0.002;
 
-parameters.infectionProbs = [0.001 0.1];
-parameters.infectionProbsJumps = 0.001;
-parameters.entryPoints = 1:16;
-parameters.numOfReps = 100;
+parameters.infectionProbs = [0.001 0.101];
+parameters.infectionProbsJumps = 0.05;
+parameters.entryPoints = 2;
+parameters.numOfReps = 2;
 
 parameters.outputFolderPath = '/shares/ulr-lab/users/daniel/networks/output';
 parameters.fileName = 'young.mat';
