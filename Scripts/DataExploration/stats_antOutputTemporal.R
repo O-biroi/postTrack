2e4a08e48043dcb9e933f6b4ba929772441c1a2d
fumaick <zimai.li@evobio.eu>
2022-04-28 23:27:30 +0200

    Add stats R code for temporal variation

---
+++ Scripts/DataExploration/stats_antOutputTemporal.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+library(EnvStats)
+library(lme4)
+library(lmerTest)
+library(DHARMa)
+library(car)
+library(multcomp)
+library(EnvStats)
+library(glmmTMB)
+
+# load data
+antOutputTemporal <- read.csv("/Users/lizimai/Desktop/postTrack/Data/outTemporalFull.csv")
+antOutputTemporal$Day = as.factor(antOutputTemporal$Day)
+
+ggplot(antOutputTemporal) +
+  geom_jitter(aes(x = Day, y = OutNestRatio, color = Treatment)) +
+  geom_boxplot(aes(x = Day, y = OutNestRatio, color = Treatment))
