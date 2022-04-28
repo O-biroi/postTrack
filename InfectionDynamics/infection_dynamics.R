c4a7eee2e2adda0b4f90f501604c81b4a7b6d46b
iMac_Zimai <zimai.li@evobio.eu>
2022-04-28 22:34:28 +0200

    Update R code

--- InfectionDynamics/infection_dynamics.R
+++ InfectionDynamics/infection_dynamics.R
@@ -6,15 +6,19 @@ library(DHARMa)
 library(car)
 library(multcomp)
 library(EnvStats)
+library(glmmTMB)
 
 # load data
 
-infectionDynamics <- read.csv("/Users/lizimai/Desktop/postTrack/InfectionDynamics/infection_dynamics.csv", sep = ";")
-infectionDynamics$rep_cycle <- as.factor(infectionDynamics$rep_cycle)
+infectionDynamics <- read.csv("/Users/zli/Desktop/postTrack/InfectionDynamics/infection_dynamics.csv", sep = ";")
+infectionDynamics$rep_cycle <- infectionDynamics$rep_cycle
 # statistics
-fit <- lmer(Infec_intensity^(1/3) ~ Caste * rep_cycle  + (1|colony), data = infectionDynamics)
+# there is zero inflation in one treatment
+fit <- glmmTMB(Infec_intensity^(1/3) ~ Caste * rep_cycle  + (1|colony),  ziformula = ~rep_cycle, data = infectionDynamics)
 testDispersion(fit)
+testZeroInflation(fit)
 simulationOutput <- simulateResiduals(fittedModel = fit, plot = F)
 plot(simulationOutput)
 Anova(fit, type = "II")
 summary(fit)
+
--- Scripts/DataExploration/stats_antOutput.R
+++ Scripts/DataExploration/stats_antOutput.R
@@ -6,21 +6,55 @@ library(DHARMa)
 library(car)
 library(multcomp)
 library(EnvStats)
+library(glmmTMB)
 
 # load data
-antOutput <- read.csv("/Users/lizimai/Desktop/postTrack/Data/antOutput.csv")
-#ßantOutput <- read.csv("/Users/lizimai/Desktop/postTrack/Data/antOutput.csv")
+antOutput <- read.csv("/Users/zli/Desktop/postTrack/Data/antOutput.csv")
+antOutput %>% 
+  mutate(TreatmentMerged = ifelse(Treatment == "C", "C", "Others")) -> antOutputTreatmentMerged
 
-fit1 <- glmer(cbind(InNestFrame, OutNestFrame) ~ Treatment  + (1|ColonyID), data = antOutput, family = binomial())
+antOutput %>% 
+  group_by(Treatment) %>% 
+  summarise(MeanOutNestRatio = mean(OutNestRatio),
+            SdOutNestRatio = sd(OutNestRatio))
+
+fit1 <- glmer(cbind(OutNestFrame, InNestFrame) ~ Treatment + InfectionLoad + (1|ColonyID), data = antOutputTreatmentMerged,  family = binomial())
 testDispersion(fit1)
 simulationOutput <- simulateResiduals(fittedModel = fit1, plot = F)
 plot(simulationOutput)
 Anova(fit1, type = "II")
 summary(fit1)
 
-fit2 <- glmer(OutNestRatio ~ Treatment  + (1|ColonyID), data = antOutput, weights = TotalFrame, family = binomial())
+
+fit2 <- glmer(cbind(OutNestFrame, InNestFrame) ~ TreatmentMerged + (1|ColonyID), data = antOutputTreatmentMerged, family = binomial())
 testDispersion(fit2)
 simulationOutput <- simulateResiduals(fittedModel = fit2, plot = F)
 plot(simulationOutput)
 Anova(fit2, type = "II")
 summary(fit2)
+
+anova(fit1, fit2)
+
+fit2 <- glmer(cbind(OutNestFrame, InNestFrame) ~ ColonyTreatmentAntInfectionStatus + (1|ColonyID), data = antOutput, family = binomial())
+testDispersion(fit2)
+simulationOutput <- simulateResiduals(fittedModel = fit2, plot = F)
+plot(simulationOutput)
+Anova(fit2, type = "II")
+summary(fit2)
+
+
+
+
+antOutputTreatmentMerged %>% 
+  filter(InfectionLoad != 0 & InfectionLoad != "NaN") -> antOutputAllInfected
+
+ggplot(antOutputAllInfected, aes(x = InfectionLoad, y = OutNestRatio, colour = Treatment)) +
+  geom_jitter()
+
+# zero inflation
+fit3 <- glmmTMB(cbind(OutNestFrame, InNestFrame) ~ InfectionLoad + (1|ColonyID),data = antOutputAllInfected,  family = binomial())
+testDispersion(fit3)
+simulationOutput <- simulateResiduals(fittedModel = fi31, plot = F)
+plot(simulationOutput)
+Anova(fit3, type = "II")
+summary(fit3)
