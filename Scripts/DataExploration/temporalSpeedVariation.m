1d269fe7d2bcbaacf8df1f99f8be08cb48ea7f99
iMac_Zimai <zimai.li@evobio.eu>
2022-04-29 13:45:46 +0200

    Split temporal speed and outNest variation into different code

--- Scripts/DataExploration/temporalSpeedVariation.m
+++ Scripts/DataExploration/temporalSpeedVariation.m
@@ -2,7 +2,6 @@
 %% Settings
 numFiles = 30;
 framesPerContinuousSegment = 12000;
-framesPerDay = 144000;
 antColour = categorical(["BB","BG","BR","GG","GR","RB","RG","RR"]'); 
 colonyID = outputFiltered.ColonyID;
 
@@ -24,41 +23,9 @@ for i = 1:numColonies
     end
 end
 
+% calculate the mean speed for each ant in each 
 
 
-%% In Nest variation
-inNestWithNansByDay = splitMergedXys(inNestWithNans, framesPerDay, 1);
-
-numColonies = length(colonyID);
-numSegments = width(inNestWithNansByDay);
-numAnts = length(antColour);
-
-sz = numColonies*numSegments*numAnts;
-vars = {'ColonyID','Colour','Day','InNestFrames','TotalFrames'};
-varTypes = {'categorical','categorical','double','double','double'};
-
-outputTemporal = table('Size',[sz,length(vars)], 'VariableTypes', varTypes, 'VariableNames', vars);
-
-for i = 1:numColonies
-    for j = 1:numSegments
-        for k = 1:numAnts
-            numRow = (i-1)*numSegments*numAnts + (j-1)*numAnts +k;
-            outputTemporal.ColonyID(numRow) = colonyID(i);
-            outputTemporal.Colour(numRow) = antColour(k);
-            outputTemporal.Day(numRow) = j;
-            outputTemporal.InNestFrames(numRow) = sum(inNestWithNansByDay{i, j}(:,k),"omitnan");
-            outputTemporal.TotalFrames(numRow) = length(inNestWithNansByDay{i, j}(:,k)) - sum(isnan(inNestWithNansByDay{i, j}(:,k)));
-        end
-    end
-end
-
-antOutputHeader = antOutput(:,["ColonyID","Treatment","Colour","InfectionLoad","InfectionStatus"]);
-outTemporalFull = join(outputTemporal, antOutputHeader);
-outTemporalFull.AssignmentRate = outTemporalFull.TotalFrames/framesPerDay;
-outTemporalFull.OutNestFrames = outTemporalFull.TotalFrames -  outTemporalFull.InNestFrames;
-outTemporalFull.OutNestRatio =outTemporalFull.OutNestFrames./outTemporalFull.TotalFrames;
-
-writetable(outTemporalFull, "outTemporalFull.csv")
 %% Plotting
 
             
