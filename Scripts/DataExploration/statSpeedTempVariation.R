ba19b78617439daeafecf120effed48e7013fdcf
fumaick <zimai.li@evobio.eu>
2022-04-29 15:58:06 +0200

    Update stat temp R code

--- Scripts/DataExploration/statSpeedTempVariation.R
+++ Scripts/DataExploration/statSpeedTempVariation.R
@@ -10,11 +10,14 @@ library(glmmTMB)
 antSpeedTemporalOrignial <- read.csv("/Users/lizimai/Desktop/postTrack/Data/outputSpeedTemporalFull.csv")
 antSpeedTemporalOrignial %>% 
   mutate(Day = as.factor(1+ floor((ContinuousSegment-1)/12)),
-         ContinuousSegment = as.factor(ContinuousSegment)) ->antSpeedTemporal
+         ContinuousSegment = as.factor(ContinuousSegment)) %>% 
+  group_by(ColonyID, Colour, Treatment, Day) %>% 
+  summarise(MeanSpeedByDay = mean(MeanSpeed, na.rm = TRUE),
+            MeanAssignmentRateByDay = mean(AssignmentRate, na.rm = TRUE))->antSpeedTemporal
 
 antSpeedTemporal %>% 
-  #ßfilter(AssignmentRate > 0.5) %>% 
+  filter(MeanAssignmentRateByDay > 0.5) %>% 
   ggplot() +
-  geom_jitter(aes(x = Day, y = MeanSpeed, color = Treatment, alpha = 0.1) ) +
-  geom_boxplot(aes(x = Day, y = MeanSpeed, color = Treatment)) +
-  stat_n_text(aes(x = Day, y = MeanSpeed))
+  geom_jitter(aes(x = Day, y = MeanSpeedByDay, color = Treatment, alpha = 0.1) ) +
+  geom_boxplot(aes(x = Day, y = MeanSpeedByDay, color = Treatment)) +
+  stat_n_text(aes(x = Day, y = MeanSpeedByDay))
