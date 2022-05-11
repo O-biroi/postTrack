4f084768afe1e5179884f9c3489f75085a2e1880
Daniel <d.m.knebel@gmail.com>
2022-05-11 10:52:42 +0200

    ...

--- network/inputs2.m
+++ network/inputs2.m
@@ -1,13 +1,13 @@
 parameters.time = datetime;
 
-parameters.filePath = '/Users/daniel/Desktop/oldClean.mat';
+parameters.filePath = '/shares/ulr-lab/users/daniel/networks/youngClean.mat';
 
 parameters.withNest = 1;
 
 parameters.contactThresh = 0.002;
 
 parameters.infectionProbs = [0.001 0.101];
-parameters.infectionProbsJumps = 0.1;
+parameters.infectionProbsJumps = 0.5;
 parameters.entryPoints = 16;
 parameters.numOfReps = 2;
 
