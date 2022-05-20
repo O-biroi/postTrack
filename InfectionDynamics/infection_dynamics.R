63cfcdb0e4029eaa2c8e608edbaa201f2cd6d927
fumaick <zimai.li@evobio.eu>
2022-05-20 19:27:40 +0200

    Update plots and stats

--- InfectionDynamics/infection_dynamics.R
+++ InfectionDynamics/infection_dynamics.R
@@ -10,11 +10,16 @@ library(glmmTMB)
 
 # load data
 
-infectionDynamics <- read.csv("/Users/zli/Desktop/postTrack/InfectionDynamics/infection_dynamics.csv", sep = ";")
+infectionDynamics <- read.csv("InfectionDynamics/infection_dynamics.csv", sep = ";")
 infectionDynamics$rep_cycle <- infectionDynamics$rep_cycle
+
+infectionDynamics %>% 
+  group_by(rep_cycle) %>% 
+  summarise(mean = mean(Infec_intensity),
+            sd = sd(Infec_intensity))
 # statistics
 # there is zero inflation in one treatment
-fit <- glmmTMB(Infec_intensity^(1/3) ~ Caste * rep_cycle  + (1|colony),  ziformula = ~rep_cycle, data = infectionDynamics)
+fit <- glmmTMB(Infec_intensity^(1/3) ~ Caste * rep_cycle  + (1|colony),  family = gaussian(), ziformula = ~rep_cycle, data = infectionDynamics)
 testDispersion(fit)
 testZeroInflation(fit)
 simulationOutput <- simulateResiduals(fittedModel = fit, plot = F)
--- Scripts/R/Nematode/StatsBehaviourInfectionLoad.R
+++ Scripts/R/Nematode/StatsBehaviourInfectionLoad.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(glmmTMB)
+library(DHARMa)
 
 AntTable <- read_csv("Data/outputAntTableMergedJoined.csv")
 
@@ -9,19 +11,39 @@ AntTable %>%
     InfectionStatus = as.factor(InfectionStatus)) %>% 
   filter(TreatmentInfectionStatus != "C_uninfected" & TreatmentInfectionStatus != "X_uninfected") %>%   group_by(ColonyID) %>% 
   group_by(ColonyID) %>% 
-  arrange(InfectionLoad) %>% 
+  arrange(InfectionLoad, ) %>% 
   mutate(RankInfectionLoad = row_number(), 
          RankInfectionLoad = ifelse(InfectionLoad == "NaN", as.numeric(NA), RankInfectionLoad)) %>% 
   arrange(OutNestRatio) %>% 
   mutate(RankOutNestRatio = row_number()) %>% 
   arrange(MeanSpeed) %>% 
-  mutate(RankMeanSpeed = row_number())-> AntTableWithRank
+  mutate(RankMeanSpeed = row_number()) %>% 
+  ungroup() -> AntTableWithRank
 
-cor.test(AntTableWithRank$RankOutNestRatio, AntTableFull$RankInfectionLoad, method = "spearman", exact = FALSE)
-cor.test(AntTableWithRank$RankMeanSpeed, AntTableFull$RankInfectionLoad, method = "spearman", exact = FALSE)
+AntTableWithRank %>% 
+  filter(TreatmentInfectionStatus == "T_infected") %>% 
+ggplot(aes(RankInfectionLoad, RankOutNestRatio)) +
+  geom_smooth(method='lm') +
+  geom_jitter()
+
+cor.test(AntTableWithRank$RankInfectionLoad, AntTableWithRank$RankOutNestRatio, method = "spearman", exact = FALSE)
+cor.test(AntTableWithRank$RankMeanSpeed, AntTableWithRank$RankInfectionLoad, method = "spearman", exact = FALSE)
+
+lmerOutLoad <- glmmTMB(OutNestRatio ~ InfectionLoad + (1|ColonyID), data = AntTableWithRank, family = beta_family())
+summary(lmerOutLoad)
+testDispersion(lmerOutLoad)
+simulationOutput <- simulateResiduals(fittedModel = lmerOutLoad, plot = F)
+plot(simulationOutput)
+
+lmerSpeedLoad <- glmmTMB(MeanSpeed ~ InfectionLoad + (1|ColonyID), data = AntTableWithRank, family = gaussian())
+summary(lmerSpeedLoad)
+testDispersion(lmerSpeedLoad)
+simulationOutput <- simulateResiduals(fittedModel = lmerSpeedLoad, plot = F)
+plot(simulationOutput)
 
   ggplot(AntTableFull, aes(color = Treatment)) +
   geom_jitter(aes(x = InfectionLoad, y = OutNestRatio), size = 2) +   
+    geom_line() +
   scale_color_manual(name = "Colony Treatment", labels = c("Infected", "Mixed"), values = c("T" = "#D95319", "X" = "#7E2F8E")) +
   xlab("Infection load (Number of nematodes)") +
   ylab("Proportion of frames being outside the nest") +
