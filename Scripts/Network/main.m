7c0bfd5f42e9847a6d9cecb81b8a1179c8ab5eb7
fumaick <zimai.li@evobio.eu>
2022-05-20 19:29:33 +0200

    Adapt network code

---
+++ Scripts/Network/main.m
@@ -0,0 +1,36 @@
+clear all
+
+inputs2
+
+disp('loadFilesGiacomo');
+tic
+[xy, inNest, parameters.numOfFiles] = loadFilesGiacomo(parameters.numOfFrames, parameters.numOfSegments, parameters.folderPath);
+toc
+
+disp('distancesClac');
+tic
+[distancesMat] = distancesClac(xy, inNest, parameters.withNest);
+toc
+
+disp('contactsCalc');
+tic
+contactsMat = contactsCalc(distancesMat, parameters.contactThresh);
+toc
+
+disp('contactRoutesCalc');
+tic
+contactsPerAnt = contactRoutesCalc(contactsMat);
+toc
+
+disp('simTempNetwork');
+i = 0;
+iterations = parameters.infectionProbs(1):parameters.infectionProbsJumps:parameters.infectionProbs(end);
+for in = iterations
+    disp (['iteration ' num2str(i) ' out of ' num2str(length(iterations))]);
+    simTempNetwork(contactsPerAnt, in, parameters.entryPoints, parameters.numOfReps, parameters.outputFolderPath);
+end
+
+clearvars i in
+% 
+% disp(['saving as: ' fullfile(parameters.outputFolderPath, parameters.fileName)])
+% save(fullfile(parameters.outputFolderPath, parameters.fileName), "infections", "parameters")
---
+++ Scripts/Network/main2.m
@@ -0,0 +1,32 @@
+clear all
+
+parpool('local', 8)
+inputsLocalTest
+
+disp('load');
+tic
+load(parameters.filePath);
+toc
+
+disp('distancesClac');
+tic
+[distancesMat] = distancesClac(xy, inNest, parameters.withNest);
+toc
+
+disp('contactsCalc');
+tic
+contactsMat = contactsCalc(distancesMat, parameters.contactThresh);
+toc
+
+disp('contactRoutesCalc');
+tic
+contactsPerAnt = contactRoutesCalc(contactsMat);
+toc
+
+disp('simTempNetwork');
+
+iterations = parameters.infectionProbs(1):parameters.infectionProbsJumps:parameters.infectionProbs(end);
+parfor in = 1:length(iterations)
+    disp (['P(infect) = ' num2str(iterations(in))]);
+    simTempNetwork(contactsPerAnt, iterations(in), parameters.entryPoints, parameters.numOfReps, parameters.outputFolderPath);
+end
