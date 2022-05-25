bab1f5ff1ade38ce744f90f5d753ab101985a8af
Daniel <d.m.knebel@gmail.com>
2022-05-25 12:09:59 +0200

    mergeInputsNew

--- network/inputsMergeOld.m
+++ network/inputsMergeOld.m
@@ -1,17 +1,17 @@
 clear all
 parpool('local', 32)
-parameters.inputFolder = '/shares/ulr-lab/Users/Daniel/outputs/outputO5';
+parameters.inputFolder = '/shares/ulr-lab/Users/Daniel/outputs/outputO6';
 parameters.time = datetime;
 parameters.numOfFrames = 12000;
 parameters.numOfFiles = 6;
 parameters.numOfSegments = 32;
-parameters.infectionProbs = [0.001 0.101];
+parameters.infectionProbs = [0.001 0.071];
 parameters.infectionProbsJumps = 0.01;
 parameters.entryPoints = 16;
-parameters.numOfReps = 20;
+parameters.numOfReps = 100;
 
 parameters.outputFolder = '/shares/ulr-lab/Users/Daniel/outputs/';
-parameters.filename = 'O5Array';
+parameters.filename = 'O6Array';
 
 
 
