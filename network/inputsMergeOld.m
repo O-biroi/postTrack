d747f75e248a08eada5a16e0b497eb3d03bc5cdc
Daniel <d.m.knebel@gmail.com>
2022-05-27 12:12:15 +0200

    ...

--- network/inputsMergeOld.m
+++ network/inputsMergeOld.m
@@ -1,6 +1,6 @@
 clear all
 parpool('local', 32)
-parameters.inputFolder = '/shares/ulr-lab/Users/Daniel/outputs/outputO6';
+parameters.inputFolder = '/shares/ulr-lab/Users/Daniel/outputs/outputO8';
 parameters.time = datetime;
 parameters.numOfFrames = 12000;
 parameters.numOfFiles = 6;
@@ -8,10 +8,12 @@ parameters.numOfSegments = 32;
 parameters.infectionProbs = [0.001 0.071];
 parameters.infectionProbsJumps = 0.01;
 parameters.entryPoints = 16;
+parameters.numOfAnts = 16;
+
 parameters.numOfReps = 100;
 
 parameters.outputFolder = '/shares/ulr-lab/Users/Daniel/outputs/';
-parameters.filename = 'O6Array';
+parameters.filename = 'O8Array';
 
 
 
--- network/inputsMergeYoung.m
+++ network/inputsMergeYoung.m
@@ -1,6 +1,6 @@
 clear all
 parpool('local', 32)
-parameters.inputFolder = '/shares/ulr-lab/Users/Daniel/outputs/outputY6';
+parameters.inputFolder = '/shares/ulr-lab/Users/Daniel/outputs/outputY8';
 parameters.time = datetime;
 parameters.numOfFrames = 12000;
 parameters.numOfFiles = 6;
@@ -8,10 +8,11 @@ parameters.numOfSegments = 32;
 parameters.infectionProbs = [0.001 0.071];
 parameters.infectionProbsJumps = 0.01;
 parameters.entryPoints = 16;
+parameters.numOfAnts = 16;
 parameters.numOfReps = 100;
 
 parameters.outputFolder = '/shares/ulr-lab/Users/Daniel/outputs/';
-parameters.filename = 'Y6Array';
+parameters.filename = 'Y8Array';
 
 
 
