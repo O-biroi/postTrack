c2c0bb41cd3e32a6389b66080c71e3995e606487
Daniel <d.m.knebel@gmail.com>
2022-06-01 16:56:23 +0200

    ...

---
+++ network/inputsDD.m
@@ -0,0 +1,14 @@
+parameters.time = datetime;
+
+parameters.filePath = '/shares/ulr-lab/users/daniel/StephanieData/DD.mat';
+
+parameters.withNest = 1;
+
+parameters.contactThresh = 0.002;
+
+parameters.infectionProbs = [0.0005 0.001];
+parameters.infectionProbsJumps = 0.0001;
+parameters.entryPoints = 1:8;
+parameters.numOfReps = 100;
+
+parameters.outputFolderPath = '/shares/ulr-lab/Users/Daniel/outputs/outputDD2';
