96eb8277627d1f79f1ca3d79608dc3b969a82fce
iMac_Zimai <zimai.li@evobio.eu>
2022-06-01 16:13:14 +0200

    update plots

--- InfectionDynamics/infection_dynamics.R
+++ InfectionDynamics/infection_dynamics.R
@@ -7,6 +7,7 @@ library(car)
 library(multcomp)
 library(EnvStats)
 library(glmmTMB)
+library(pbkrtest)
 
 # load data
 
@@ -14,11 +15,24 @@ infectionDynamics <- read.csv("/Users/zli/Desktop/postTrack/InfectionDynamics/in
 infectionDynamics$rep_cycle <- infectionDynamics$rep_cycle
 # statistics
 # there is zero inflation in one treatment
-fit <- glmmTMB(Infec_intensity^(1/3) ~ Caste * rep_cycle  + (1|colony),  ziformula = ~rep_cycle, data = infectionDynamics)
+gmm1 <- glmmTMB(Infec_intensity^(1/3) ~ Caste * rep_cycle  + (1|colony),  ziformula = ~rep_cycle, data = infectionDynamics)
 testDispersion(fit)
 testZeroInflation(fit)
 simulationOutput <- simulateResiduals(fittedModel = fit, plot = F)
 plot(simulationOutput)
 Anova(fit, type = "II")
 summary(fit)
+
+gmm2 <- update(gmm1, .~.-Caste : rep_cycle)
+anova(gmm1, gmm2)
+PBmodcomp(gmm1, gmm2, cl=2)
+
+drop1(gmm1, scope = ~Caste : rep_cycle, scale = 0, test = "Chisq" )
+drop1(gmm1, scope = ~Caste, scale = 0, test = "Chisq" )
+drop1(gmm1, scope = ~rep_cycle, scale = 0, test = "Chisq" )
+
+
+
+drop1(fit, scale = 0, test = "Chisq" )
+drop1(fit, scale = 0, test = "Chisq" )
 
--- Scripts/R/Nematode/StatsBehaviourInfectionLoad.R
+++ Scripts/R/Nematode/StatsBehaviourInfectionLoad.R
@@ -3,8 +3,9 @@ library(tidyverse)
 AntTable <- read_csv("Data/outputAntTableMergedJoined.csv")
 InfectionStatus <- read_delim("Data/antInfectionStatus.csv", delim = ";")
 
-left_join(AntTable, InfectionStatus, by = c("ColonyID", "Colour", "InfectionStatus")) %>% 
-  mutate(
+#left_join(, by = c("ColonyID", "Colour", "InfectionStatus")) %>% 
+  AntTable %>% 
+   mutate(
     TreatmentInfectionStatus = as.factor(paste0(Treatment, "_", InfectionStatus)),
     Treatment = as.factor(Treatment),
     InfectionStatus = as.factor(InfectionStatus)) %>% 
