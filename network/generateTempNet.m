65edd59df705c6aa0c5e63bea6fad8f451c2cecc
Daniel <d.m.knebel@gmail.com>
2022-06-04 18:38:04 +0200

    ...

---
+++ network/generateTempNet.m
@@ -0,0 +1,32 @@
+function generateTempNet(genotype)
+    load('/shares/ulr-lab/Users/Daniel/netowrks/contactsMat')
+    template = eval(genotype);
+    numOfSteps = 12000;
+    numOfNets2Produce = 10;
+
+    parpool('local', numOfNets2Produce)
+
+    numOfNodes = size(template{1, 1}, 1);
+    emptyMat = zeros([numOfNodes, numOfNodes, numOfSteps], "logical");
+    meetingsMat = cell(size(template, 1), size(template, 2), numOfNets2Produce);
+    meetingsMat(cellfun(@isempty, meetingsMat)) = {emptyMat};
+    % meetingsMat(cellfun(@isempty, template), :) = {nan};
+    for i = 1:size(template, 1)
+        for in = 1:size(template, 2)
+            relvantTemp = template{i, in};
+            if isempty(relvantTemp)
+                meetingsMat{i, in, :} = {nan};
+                break
+            end
+            parfor ind = 1:numOfNets2Produce
+                for inde = 2:numOfNodes
+                    for index = 1:inde-1
+                        randVec = randi(numOfSteps, relvantTemp(inde, index));
+                        meetingsMat{i, in, ind}(inde, index, randVec) = 1;
+                    end
+                end
+            end
+        end
+    end
+    save(fullfile('/shares/ulr-lab/Users/Daniel/output/', [genotype 'RandomizedNetworks']))
+end
