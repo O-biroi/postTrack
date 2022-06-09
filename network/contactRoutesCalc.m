44cc55ba2d0e896580ea75a18106c70dbd1c81d8
Daniel <d.m.knebel@gmail.com>
2022-06-09 11:30:15 +0200

    ...

--- network/contactRoutesCalc.m
+++ network/contactRoutesCalc.m
@@ -1,7 +1,7 @@
 function contactsPerAnt = contactRoutesCalc(contactsMat)
 
-    numOfAnts = size(contactsMat{1}(:,:,1), 1);
-
+%     numOfAnts = size(contactsMat{1}(:,:,1), 1);
+    numOfAnts = size(contactsMat{find(~cellfun(@isempty, contactsMat), 1, 'first')}(:,:,1), 1);
     %% Generate masking matrices
     % for working only on one copy of couples - the lower part of the
     % contactsMat.
