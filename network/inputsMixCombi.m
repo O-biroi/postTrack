b76e7ec183fd7a1a232c41e0af44cf2d2771b108
Daniel <d.m.knebel@gmail.com>
2022-06-18 04:46:08 +0200

    ...

---
+++ network/inputsMixCombi.m
@@ -0,0 +1,14 @@
+parameters.time = datetime;
+
+parameters.filePath = '/shares/ulr-lab/users/daniel/networks/mixClean.mat';
+
+parameters.withNest = 1;
+
+parameters.contactThresh = 0.002;
+
+parameters.infectionProbs = [0.0007];
+parameters.infectionProbsJumps = 0.01;
+parameters.entryPoints = 1:16;
+parameters.numOfReps = 20;
+
+parameters.outputFolderPath = '/shares/ulr-lab/Users/Daniel/outputs/outputCM1';
