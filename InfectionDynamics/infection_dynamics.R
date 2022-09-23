c17e4d8386403092fb26f139f38dd1e607a14cf6
iMac_Zimai <zimai.li@evobio.eu>
2022-09-23 10:48:03 +0200

    Update statistics

--- InfectionDynamics/infection_dynamics.R
+++ InfectionDynamics/infection_dynamics.R
@@ -8,36 +8,56 @@ library(multcomp)
 library(EnvStats)
 library(glmmTMB)
 library(pbkrtest)
+library(emmeans)
+library(ggplot2)
 
-# load data
+# load data
 
 infectionDynamics <- read.csv("InfectionDynamics/infection_dynamics.csv", sep = ";")
 infectionDynamics$rep_cycle <- infectionDynamics$rep_cycle
 
+# prepare data for infection probability
 infectionDynamics %>% 
-  group_by(rep_cycle) %>% 
-  summarise(mean = mean(Infec_intensity),
-            sd = sd(Infec_intensity))
+  filter(rep_cycle == 1) %>% 
+  mutate(infection_status = Infec_intensity != 0) -> infectionStatusCycle1
+  
 # statistics
-# there is zero inflation in one treatment
-gmm1 <- glmmTMB(Infec_intensity^(1/3) ~ Caste * rep_cycle  + (1|colony),  ziformula = ~rep_cycle, data = infectionDynamics)
+# logistic regression for infection probability at colony cycle 1
+glmer <- glmer(data = infectionStatusCycle1, infection_status ~ Caste + (1|colony), family = binomial())
+testDispersion(glmer)
+testZeroInflation(glmer)
+simulationOutput <- simulateResiduals(fittedModel = glmer, plot = F)
+plot(simulationOutput)
+summary(glmer)
+drop1(glmer, scope = ~Caste, scale = 0,test = "Chisq" )
+
+# post-hoc compare probabilities
+emm <- emmeans(glmer, "Caste", type = "response")
+emm
+pairs(emm)
+
+# glmm for infection intensity
+## there is zero inflation in one treatment
+fit <- glmmTMB(Infec_intensity ~ Caste + rep_cycle + Caste:rep_cycle  + (1|colony),  ziformula = ~rep_cycle, data = infectionDynamics)
 testDispersion(fit)
 testZeroInflation(fit)
 simulationOutput <- simulateResiduals(fittedModel = fit, plot = F)
 plot(simulationOutput)
-Anova(fit, type = "II")
-summary(fit)
+drop1(fit, scope = ~Caste : rep_cycle, scale = 0,test = "Chisq" )
 
-gmm2 <- update(gmm1, .~.-Caste : rep_cycle)
-anova(gmm1, gmm2)
-PBmodcomp(gmm1, gmm2, cl=2)
-
-drop1(gmm1, scope = ~Caste : rep_cycle, scale = 0, test = "Chisq" )
-drop1(gmm1, scope = ~Caste, scale = 0, test = "Chisq" )
-drop1(gmm1, scope = ~rep_cycle, scale = 0, test = "Chisq" )
+# compare slopes
+emt <- emtrends(fit, ~Caste|rep_cycle, var = "rep_cycle")
+emt          # estimated slopes for each species
+pairs(emt)   # pairwise comparisons of slopes
 
 
+# pairwise comparison between foragers and nurses at different time points (not appropriate since time is continuous - lead to estimation higher than real values for t = 2)
+pairwiseFit <- emmeans(fit, pairwise~Caste|rep_cycle, at = list(rep_cycle = c(1,2,3,4)))
+pairwiseFit
 
-drop1(fit, scale = 0, test = "Chisq" )
-drop1(fit, scale = 0, test = "Chisq" )
+pairwiseFit2 <- emmeans(fit, pairwise~rep_cycle, at = list(rep_cycle = c(1,2,3,4)))
+pairwiseFit2
+
+
+
 
